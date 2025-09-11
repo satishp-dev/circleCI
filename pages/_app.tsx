0235cda4928a911041c2b0b6a8f374bd71164030
satishp-dev <158128227+satishp-dev@users.noreply.github.com>
2025-09-11 18:15:25 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,5 @@ import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
    console.log('gdhsgdhgsdhg')
-
    return <Component {...pageProps} />; 
  }
