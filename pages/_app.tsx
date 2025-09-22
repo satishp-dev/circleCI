cb2ea0deff362a97bd06731b985f86bc049e4267
satishp-dev <158128227+satishp-dev@users.noreply.github.com>
2025-09-22 11:51:24 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
+   console.log('satish')
 
    return <Component {...pageProps} />; 
  }
