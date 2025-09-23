b8ae480fb493bc3c89cb81f56945d7b79c2211f9
satishp-dev <158128227+satishp-dev@users.noreply.github.com>
2025-09-23 18:34:59 +0530

    chore(ai-refactor): applied suggestion in pages/_app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,6 @@ import logger from 'winston';
 
 export default function App({ Component, pageProps }: AppProps) {
    // console.log('ksdskdjksjdkdj') // Consider removing this line or replace it with a proper logging mechanism
-   logger.error('error');
+   // console.error('error') // Consider removing this line or replace it with a proper error handling mechanism
    return <Component {...pageProps} />; 
  }
