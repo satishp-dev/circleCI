f4c196839af0c42fbb91ce56c5e4bc35651c0597
satishp-dev <158128227+satishp-dev@users.noreply.github.com>
2025-09-23 18:32:52 +0530

    chore(ai-refactor): applied suggestion in pages/_app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import logger from 'winston';
 
 
 export default function App({ Component, pageProps }: AppProps) {
-   console.log('ksdskdjksjdkdj')
-   console.error('error')
+   logger.info('ksdskdjksjdkdj');
+   logger.error('error');
    return <Component {...pageProps} />; 
  }
