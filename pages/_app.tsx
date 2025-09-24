892c4a8b930f587add7cb3aa96929c59d60d267c
satihp-dev <satishp@onetab.ai>
2025-09-24 17:46:51 +0530

    chore(logging): remove commented-out logging statements in _app.tsx and _document.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-   // Replace with proper logging mechanism
-   // logger.info('ssjjsdkskdskdjksjdjksj')
-   // logger.error('dksdjksjdksjdsdk')
+
    
    return <Component {...pageProps} />; 
  }
