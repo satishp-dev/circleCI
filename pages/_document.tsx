ff6e50e83267739858e0d92b58f166e5e433714b
satishp-dev <158128227+satishp-dev@users.noreply.github.com>
2025-09-24 12:36:46 +0530

    chore(ai-refactor): applied suggestion in pages/_document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  console.log('Document loaded');
+  // console.log('Document loaded');
   return (
     <Html lang="en">
       <Head />
