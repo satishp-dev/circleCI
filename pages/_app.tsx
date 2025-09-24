286b3ba5666ce65ae359d488f71d50cf69236fb1
satishp-dev <158128227+satishp-dev@users.noreply.github.com>
2025-09-24 11:05:00 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import logger from 'winston';
+
 
 
 export default function App({ Component, pageProps }: AppProps) {
-   // console.log('ksdskdjksjdkdj') // Consider removing this line or replace it with a proper logging mechanism
-   // console.error('error') // Consider removing this line or replace it with a proper error handling mechanism
+   console.log('ssjjsdkskdskdjksjdjksj')
+   console.error('dksdjksjdksjdsdk')
    return <Component {...pageProps} />; 
  }
