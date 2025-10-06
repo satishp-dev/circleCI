ccf028c1569c39f895e5fd812e0210b7dcbebcf6
satihp-dev <satishp@onetab.ai>
2025-10-06 16:45:44 +0530

    s

--- pages/index.tsx
+++ pages/index.tsx
@@ -96,7 +96,7 @@ export default function Home() {
           />
           Examples
         </a>
-        <a
+        <acccccccccccccc
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
           target="_blank"
