e84528214b5c00542690c132b5d8deae3ec0047b
satihp-dev <satishp@onetab.ai>
2025-10-06 19:09:15 +0530

    aaaaaaa

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,6 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-   // console.log('djksjdksjd') // Commented out for production
+   console.log('djksjdksjd') // Commented out for production
    return <Component {...pageProps} />;  
  }
