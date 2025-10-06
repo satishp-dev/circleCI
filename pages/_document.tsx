32772e27008cb56355798b98827a950a430a3191
satihp-dev <satishp@onetab.ai>
2025-10-06 16:23:23 +0530

    jjhj

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  console.warn("Documentrendered"); 
+  console.warn("Documen trendered"); 
   return (
     <Html lang="en">
       <Head />
