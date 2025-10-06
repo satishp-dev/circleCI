818d97e3ac21e28f9f25eb6e0204114ef17f153c
satishp-dev <158128227+satishp-dev@users.noreply.github.com>
2025-10-06 10:39:02 +0530

    chore(ai-refactor): applied suggestion in pages/_app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,4 +4,4 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
    console.error('djksjdksjd')  
    return <Component {...pageProps} />; 
- }   
+ }
