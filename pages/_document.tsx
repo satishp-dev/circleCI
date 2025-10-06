01f300aa81615d82f65767d9d7b1ae476b5148eb
satihp-dev <satishp@onetab.ai>
2025-10-06 12:24:47 +0530

    Fix console log message formatting in _document.tsx and index.tsx for consistency

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  console.warn("Document rendere d"); 
+  console.warn("Document rendered"); 
   return (
     <Html lang="en">
       <Head />
