61111538dfa5ea1e49769e326eea40e8aee6357f
satihp-dev <satishp@onetab.ai>
2025-10-07 14:41:19 +0530

    check

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,4 +4,4 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
    // console.log('djksjdksjdapp') // Commented out for production
    return <Component {...pageProps} />;  
-  }
+  }.    
