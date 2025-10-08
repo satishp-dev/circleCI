73949b6d9cac7dc9ee030ca766ca45b5472dec29
satishp-dev <158128227+satishp-dev@users.noreply.github.com>
2025-10-08 11:44:02 +0530

    chore(ai-refactor): applied suggestion in pages/_document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  // Use a logging library like winston or pino
   return (
     <Html lang="en">
       <Head />
