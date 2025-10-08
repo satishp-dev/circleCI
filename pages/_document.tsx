ca4df449e644da730d748ced890f651425bfd3c6
satihp-dev <satishp@onetab.ai>
2025-10-08 12:19:39 +0530

    update99

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,14 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  // console.log('Document rendered')
+  console.log('Document rendered')
   return (
     <Html lang="en">
       <Head />
       <body className="antialiased">
         <Main />
         <NextScript />
+        ccc
       </body>
     </Html>
   );
