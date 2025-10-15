2f1a4d8ed01393d3a14c8b29a2e9af2467a0329f
satishp-dev <158128227+satishp-dev@users.noreply.github.com>
2025-10-15 15:17:40 +0530

    chore(ai-refactor): applied suggestion in pages/_app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,3 +5,5 @@ export default function App({ Component, pageProps }: AppProps) {
    // Insert meaningful code here
    return <Component {...pageProps} />;
 }
+
+// Add a newline at the end of the file
