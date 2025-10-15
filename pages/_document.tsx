80c848ad07d7687be7c6c8650be41486cc6c84bb
satihp-dev <satishp@onetab.ai>
2025-10-15 12:15:59 +0530

    update app and document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,8 @@ import React from "react";
 
 export default function Document() {
   React.useEffect(() => {
-    
+    'kmldjgldflgjdfls'g'
+    'ffgdfkg'kfdg'k'dfgk'fkdg'
   }, []);
   return (
     <Html lang="en">
