089bee24810f90f8e918d5133531502d1d661860
satishp-dev <158128227+satishp-dev@users.noreply.github.com>
2025-11-20 16:16:00 +0530

    chore(ai-refactor): applied suggestion in pages/_document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Document() {
   React.useEffect(() => {
-    console.log("Document component rendered")
+    // console.log("Document component rendered")
   }, []);
   return (
     <Html lang="en">
