c426661b96ee846a7a6895557a94d84494e452f9
Gautam-chouhan <gautamchouhan@onetab.ai>
2025-11-24 18:02:20 +0530

    gautam postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,15 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
+let person_age=100;
 
-export default config;
+if(person_age>=18)
+{
+
+console.log("Eligible for vote");
+
+}
+else{
+
+    console.log("Not Eligible for vote")
+
+}
+console.log("program exicuted.....");
