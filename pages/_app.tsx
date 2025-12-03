3f89b4c1dea61e3d4e1b9d96cc9a77956a1ad2d7
satishp-dev <>
2025-12-03 10:29:43 +0530

    fix: remove duplicate log statement in App component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
    // Insert meaningful code here
    console.log("App component rendered")
+   console.log("App component rendered")
    return <Component {...pageProps} />;
 }
 
