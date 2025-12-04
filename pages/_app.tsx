0e901551152d32218c62b6a4076241d4a7b9bd56
satishp-dev <>
2025-12-04 12:35:01 +0530

    test8

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,7 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
    // Insert meaningful code here
 
-   console.log('fhujjfhbheu') 
-   
-
+   console.log('fhujjfhbheu')
    // kjfjfljflejf
 
    return <Component {...pageProps} />;
