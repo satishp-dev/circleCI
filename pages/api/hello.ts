0bfc78a32436a005b0152a499713a864284eb68e
abhilash-onetab <165123633+abhilash-onetab@users.noreply.github.com>
2026-05-04 20:38:43 +0530

    Refactor console logging in hello.ts
    Removed loop and added multiple console logs for data.

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -5,9 +5,9 @@ type Data = {
   name: string;
 };
 
-for (let i = 0; i < 9; i++) {
-   console.log("print data", Data);
-}
+   console.log("print data");
+  console.log("print data");
+  console.log("print data");
 
 export default function handler(
   req: NextApiRequest,
