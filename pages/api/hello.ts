caab4416d30a505e327756372f1b2d0556bb25bf
abhilash-onetab <165123633+abhilash-onetab@users.noreply.github.com>
2026-05-04 18:03:23 +0530

    Remove console log from API handler
    Commented out console log for production.

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -4,7 +4,6 @@ import type { NextApiRequest, NextApiResponse } from "next";
 type Data = {
   name: string;
 };
-// console.log("data====>", data) // Commented out for production
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
