436853a7362b9d1d1933755e5d403e31851c24b8
satishp-dev <158128227+satishp-dev@users.noreply.github.com>
2026-05-05 16:53:16 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import React from "react";
+import React from "react"; 
 
 export default function Document() { 
   /* Runs once after component mounts to log render */
