c037aeef0f089e87aa7aeece6c2195d5a968a3e0
satishp-dev <158128227+satishp-dev@users.noreply.github.com>
2026-05-05 10:03:40 +0530

    Clean up console.log statements in hello.ts
    Remove duplicate console.log statements.

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -5,6 +5,9 @@ type Data = {
   name: string;
 }; 
 console.log("data=======> x4", Data)
+console.log("data=======> x4")
+console.log("data=======> x4")
+console.log("data=======> x4")
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
