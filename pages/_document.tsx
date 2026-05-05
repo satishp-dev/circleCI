7680b9f134489081b2e2eb9564d1e31a7a3a432f
satishp-dev <158128227+satishp-dev@users.noreply.github.com>
2026-05-05 17:42:39 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 import React from "react";    
 
 export default function Document() { 
-  /* Runs once after component mounts to log render */
+  /* Runs once after component mounts to log render */ 
   React.useEffect(() => {  
     console.log('testhhing')     
   }, []);      
