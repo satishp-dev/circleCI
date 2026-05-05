6e0187099148e10f4f929a59960184fcbe25c366
niharkushwah1 <151032202+niharkushwah1@users.noreply.github.com>
2026-05-05 17:55:40 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";     
 import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) { 
-   // Insert meaningful code here\  
+   // Insert meaningful code here\
 
    for (let i = 0; i < 4; i++) {
      console.log('duhdusdhuihsi', "data");
